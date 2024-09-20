56ee6810d944041ca755dd63e712f2186e2261a2
cjsqudwns <jd8795@naver.com>
2024-09-20 15:13:38 +0900

    init

--- src/main/java/org/konkuk/foodmeter/FoodmeterApplication.java
+++ src/main/java/org/konkuk/foodmeter/FoodmeterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodmeterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FoodmeterApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmeterApplication.class, args);
+	}
 
 }
--- src/test/java/org/konkuk/foodmeter/FoodmeterApplicationTests.java
+++ src/test/java/org/konkuk/foodmeter/FoodmeterApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodmeterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
